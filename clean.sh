9fe28b0438a997c8c5efe424ec5fef8c78dd1197
vsapronov <vladimir.sapronov@gmail.com>
2013-09-29 23:52:46 -0400

    improved clean procedure

--- clean.sh
+++ clean.sh
@@ -2,6 +2,10 @@
 
 apt-get -y remove owncloud
 rm -rf /var/www/owncloud
-apt-get -y remove mysql-server mysql-client mysql-common
-sudo rm -rf /var/lib/mysql
+rm -rf /data/owncloud
+rm -rf /etc/owncloud
+apt-get -y purge mysql-common mysql-server-5.5
+rm -rf /var/lib/mysql
+rm -rf /data/mysql
+rm -rf /etc/mysql
 apt-get -y autoremove
