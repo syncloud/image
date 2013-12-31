0b8bd88af20ee56c309bf0635e3cedd5026f30c9
vsapronov <vladimir.sapronov@gmail.com>
2013-12-31 11:44:17 -0500

    First version of cubieboard 1 linaro tweak script

--- cubieboard.sh
+++ cubieboard.sh
@@ -1,16 +1,30 @@
 # update packages
 apt-get update
 
-# fix mac address
--> get mac address by ifconfig locate eth0 HWaddr
-cd /boot
-nano script.fex
---> add following text to the end
---> [dynamic]
---> MAC = "MAC ADDRESS"
-fex2bin script.fex script.bin
+WORKING_FOLDER=/tmp/syncloud
 
-# install disk
+# packages for fex boot adjustment
+apt-get install build-essential git
+mkdir $WORKING_FOLDER
+cd $WORKING_FOLDER
+git clone https://github.com/linux-sunxi/sunxi-tools
+cd sunxi-tools
+make bin2fex
+make fex2bin
+
+# generate and fix mac address
+mkdir /mnt/nanda
+mount /dev/nanda /mnt/nanda
+cp /mnt/nanda/script.bin $WORKING_FOLDER/script.bin
+cd $WORKING_FOLDER
+bin2fex script.bin script.fex
+MAC_ADDRESS=$(echo dd if=/dev/urandom bs=1024 count=1 2>/dev/null|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\)\(..\).*$/00\2\3\4\5\6/')
+sed -i "$ a\[dynamic]\nMAC = \"$MAC_ADDRESS\"" script.fex
+fex2bin script.fex script.bin
+cp $WORKING_FOLDER/script.bin /mnt/nanda/script.bin
+sync
+
+# mount disk
 cd /
 mkdir data
---> modify /etc/fstab - add /dev/sda1 to /data
+sed -i '$ a\/dev/sda1 /data ext4 defaults 0 0' /etc/fstab
