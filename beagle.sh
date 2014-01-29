b88d06c858ba0106bfc0cd5f443c18d47a4484ca
Boris Rybalkin <ribalkin@gmail.com>
2014-01-29 22:46:30 +0000

    initial version of beagle flasher creator

---
+++ beagle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+mkdir /data
+
+wget -qO- https://raw.github.com/syncloud/owncloud-setup/master/owncloud.sh | sudo bash
+
+sed -i '/debugfs.*fstab/a \\techo "/dev/sda1 /data auto defaults 0 0" >> /tmp/rootfs/etc/fstab' /opt/scripts/tools/beaglebone-black-eMMC-flasher.sh
+/opt/scripts/tools/beaglebone-black-eMMC-flasher.sh
