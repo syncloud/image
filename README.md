137d1488c8b9309f994be4e82dd2d502213f8c62
Boris Rybalkin <ribalkin@gmail.com>
2014-01-29 23:52:54 +0000

    switching beagle back to master

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ````
 wget -qO- https://raw.github.com/syncloud/owncloud-setup/master/cubieboard.sh | sudo bash
-wget -qO- https://raw.github.com/syncloud/owncloud-setup/internal_mysql/beagle.sh | sudo bash
+wget -qO- https://raw.github.com/syncloud/owncloud-setup/master/beagle.sh | sudo bash
 wget -qO- https://raw.github.com/syncloud/owncloud-setup/master/owncloud.sh | sudo bash
 ````
--- beagle.sh
+++ beagle.sh
@@ -7,7 +7,7 @@ fi
 
 mkdir /data
 
-wget -qO- https://raw.github.com/syncloud/owncloud-setup/internal_mysql/owncloud.sh | sudo bash
+wget -qO- https://raw.github.com/syncloud/owncloud-setup/master/owncloud.sh | sudo bash
 
 sed -i '/debugfs.*fstab/a \\techo "/dev/sda1 /data auto defaults 0 0" >> /tmp/rootfs/etc/fstab' /opt/scripts/tools/beaglebone-black-eMMC-flasher.sh
 /opt/scripts/tools/beaglebone-black-eMMC-flasher.sh
