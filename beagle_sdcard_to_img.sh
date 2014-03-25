712ff36a94295b9e1b503db80efd4cfaca47e47a
Boris Rybalkin <ribalkin@gmail.com>
2014-03-25 23:41:41 +0000

    raspberry script

--- beagle_sdcard_to_img.sh
+++ beagle_sdcard_to_img.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 e2fsck -f -y -v /dev/mmcblk0p2
-resize2fs /dev/mmcblk0p2 1300000K
+resize2fs /dev/mmcblk0p2 1300M
 dd if=/dev/mmcblk0 of=./beagle.img bs=1M count=1500
 xz -z beagle.img
