902659ba01c02d39306a23f765dd892f74941f50
Boris Rybalkin <ribalkin@gmail.com>
2014-03-26 00:50:43 +0000

    increasing free space on raspberry

--- raspberry_sdcard_to_img.sh
+++ raspberry_sdcard_to_img.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 e2fsck -f -y -v /dev/mmcblk0p2
-resize2fs /dev/mmcblk0p2 2700M
-dd if=/dev/mmcblk0 of=./raspberrypi.img bs=1M count=2800
+resize2fs /dev/mmcblk0p2 3000M
+dd if=/dev/mmcblk0 of=./raspberrypi.img bs=1M count=3100
 xz -z raspberrypi.img
