389af471df2b5626f536a940e8a198a24135ae12
Boris Rybalkin <ribalkin@gmail.com>
2014-04-05 00:20:43 +0100

    initial device ci build

---
+++ ci/build-image.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+if [[ $(uname -n) == "raspberrypi" ]]; then
+  USER=pi
+  IMAGE_FILE=2014-01-07-wheezy-raspbian
+  IMAGE_FILE_ZIP=$IMAGE_FILE.zip
+  IMAGE_URL=https://rcn-ee.net/deb/flasher/saucy/$IMAGE_FILE_ZIP
+  UNZIP=unzip
+elif [[ $(uname -n) == "arm" ]]; then
+  USER=ubuntu
+  IMAGE_FILE=BBB-eMMC-flasher-ubuntu-13.10-2014-02-16-2gb.img
+  IMAGE_FILE_ZIP=$IMAGE_FILE.xz
+  IMAGE_URL=https://rcn-ee.net/deb/flasher/saucy/$IMAGE_FILE_ZIP
+  UNZIP=unxz
+fi
+
+cd /data
+mkdir -p syncloud
+cd syncloud
+
+apt-get install xz-utils git makeself
+
+git clone https://github.com/syncloud/owncloud-setup
+cd owncloud-setup
+./build.sh
+cd ..
+
+#wget $IMAGE_URL
+#$UNZIP $IMAGE_FILE_ZIP
+#fdisk $IMAGE_FILE
+STARTSECTOR=$(file $IMAGE_FILE | grep -oP 'partition 2.*startsector \K[0-9]*(?=, )')
+losetup -o $(($STARTSECTOR*512)) /dev/loop0 $IMAGE_FILE
+mkdir image
+mount /dev/loop0 image
+cp owncloud-setup/syncloud_setup.sh image/home/$USER
+chroot image
+cd /home/$USER
+
