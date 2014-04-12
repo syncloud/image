fedb5a1fe0330ebe1eb1d838a60c07cc48b05ddb
Boris Rybalkin <ribalkin@gmail.com>
2014-04-12 22:20:05 +0100

    no internet - no build

--- ci/ci.sh
+++ ci/ci.sh
@@ -24,6 +24,11 @@ cd $BUILD_DIR
 GIT_URL=https://github.com/syncloud/owncloud-setup
 REV_FILE=$CI_DIR/.revision
 LATEST_REV=$(git ls-remote $GIT_URL refs/heads/master | cut -f1)
+if [ "$LATEST_REV" == "" ]; then
+  echo "Unable to get latest version"
+  exit 1
+fi
+
 if [ -f $REV_FILE ]; then
   CURRENT_REV=$(<$REV_FILE)
   if [ "$CURRENT_REV" == "$LATEST_REV" ]; then
