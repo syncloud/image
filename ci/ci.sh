084b39a82a61700a50bcc124632a05c90c720073
Boris Rybalkin <ribalkin@gmail.com>
2014-04-29 00:30:14 +0100

    trying to build cubieboard on beagleboard

--- ci/ci.sh
+++ ci/ci.sh
@@ -42,7 +42,13 @@ fi
 echo "$LATEST_REV" > $REV_FILE
 echo "Build triggered for rev: $LATEST_REV" > $BUILD_LOG
 
+SYNCLOUD_BOARD=$(uname -n)
 wget -qO- https://raw.github.com/syncloud/owncloud-setup/master/ci/build-image.sh | exec -a syncloud-job bash >> $BUILD_LOG 2>&1
+
+if [ $SYNCLOUD_BOARD == "arm" ]; then
+  SYNCLOUD_BOARD="cubieboard"
+  wget -qO- https://raw.github.com/syncloud/owncloud-setup/master/ci/build-image.sh | exec -a syncloud-job bash >> $BUILD_LOG 2>&1
+fi
 
 #if [ $? -nq 0 ]; then
 #  echo "Build failed" >> $BUILD_LOG
