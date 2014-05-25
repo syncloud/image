79697a27aef10272e65afbcf32fc49682987422e
Boris Rybalkin <ribalkin@gmail.com>
2014-05-25 12:04:41 +0100

    adding insider and owncloud-ctl

--- scripts/owncloud.sh
+++ scripts/owncloud.sh
@@ -132,7 +132,9 @@ sed -i '/# By default this script does nothing./a '$BOOT_SCRIPT_NAME /etc/rc.loc
 echo "root:syncloud" | chpasswd
 
 wget -qO- https://raw.githubusercontent.com/syncloud/apps/master/spm | bash -s install
+/opt/syncloud/repo/system/spm install insider
 /opt/syncloud/repo/system/spm install owncloud
+/opt/syncloud/repo/system/spm install owncloud-ctl
 
 
 
