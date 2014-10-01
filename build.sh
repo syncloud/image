885742f2edfc024b6471457820f21d2b3cd4c520
Boris Rybalkin <ribalkin@gmail.com>
2014-10-01 21:55:41 +0100

    removing version build packager

--- build.sh
+++ build.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-BUILDDIR=scripts/
-git rev-parse --short HEAD > $BUILDDIR/version
-makeself --notemp $BUILDDIR syncloud-setup.sh "The syncloud setup script" ./syncloud.sh
+makeself --notemp scripts/ syncloud-setup.sh "The syncloud setup script" ./syncloud.sh
