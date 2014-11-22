9ac877a7e5a9d402ddb5650b00e708683ed90c89
Boris Rybalkin <ribalkin@gmail.com>
2014-11-22 13:03:45 +0000

    pre-install image-tools, moving chroot specific commands to chroot prepare scripts

--- disable-service-restart.sh
+++ disable-service-restart.sh
@@ -5,3 +5,8 @@ cat <<NOSTART > /usr/sbin/policy-rc.d
 exit 101
 NOSTART
 chmod +x /usr/sbin/policy-rc.d
+
+#Fix debconf frontend warnings
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export DEBCONF_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive
--- syncloud.sh
+++ syncloud.sh
@@ -10,17 +10,13 @@ fi
 set -x
 #export SHELLOPTS
 
-#Fix debconf frontend warnings
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-export DEBCONF_FRONTEND=noninteractive
-export DEBIAN_FRONTEND=noninteractive
-
 wget -qO- https://raw.githubusercontent.com/syncloud/apps/0.7/sam | bash -s bootstrap
 
-sam --debug install image-base
-sam --debug install image-boot
+sam install image-base
+sam install image-boot
+sam install image-tools
 sam install insider
-sam --debug install owncloud
+sam install owncloud
 sam install owncloud-ctl
 sam install discovery
 sam install remote-access
