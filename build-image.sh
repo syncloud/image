100539279aabb91f6ca0ca75914b3036d991264b
Boris Rybalkin <ribalkin@gmail.com>
2014-11-26 00:03:08 +0000

    using proper partition id if any

--- build-image.sh
+++ build-image.sh
@@ -100,8 +100,7 @@ elif [[ ${PLATFORM} == "x86_64" ]]; then
   INIT_RANDOM=false
 
   #Get real kernel for guestfish on Travis VM
-  apt-cache search linux-image
-  apt-get install -y linux-image-3.13.0-39-generic
+  update-guestfs-appliance
 
 fi
 
@@ -184,8 +183,7 @@ sudo chmod 0666 /dev/kvm
 echo "fixing kernel rights"
 chmod +r /boot/vmlinuz*
 
-update-guestfs-appliance
-guestfish --ro -a $SYNCLOUD_IMAGE -m /dev/sda2 <<EOF
+guestfish --ro -a $SYNCLOUD_IMAGE -m /dev/sda${PARTITION} <<EOF
 copy-out / ./$IMAGE_FOLDER
 EOF
 
@@ -260,7 +258,7 @@ fi
 #echo "removing loop device"
 #losetup -d $LOOP_DEVICE
 
-guestfish --rw -a $SYNCLOUD_IMAGE -m /dev/sda2 <<EOF
+guestfish --rw -a $SYNCLOUD_IMAGE -m /dev/sda${PARTITION} <<EOF
 copy-in ./$IMAGE_FOLDER /
 EOF
 
