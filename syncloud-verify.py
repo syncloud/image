aa6ebf2b287506e5eb53b06ff50cccceff45f879
Boris Rybalkin <ribalkin@gmail.com>
2015-01-30 21:50:53 +0000

    activate with server for verification

--- syncloud-verify.py
+++ syncloud-verify.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 import logging
-from syncloud.apache.facade import ApacheFacade
 from syncloud.app import logger
 from syncloud.insider.facade import get_insider
 from syncloud.owncloud import facade
+from syncloud.server.serverfacade import get_server
 
 logger.init(logging.DEBUG, True)
 
-apache = ApacheFacade()
-apache.activate('localhost')
+server = get_server()
+release = open('RELEASE', 'r').read().strip()
+server.activate(release, 'localhost', 'http://api.syncloud.info:81', 'mail@example.com', 'test', 'test')
 
 owncloud = facade.get_control(get_insider())
 owncloud.finish('test', 'test', 'localhost', 'http')
