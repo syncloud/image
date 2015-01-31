d3859627c2d7ae8f8ef8fe845fd2e702b1d26ba8
Boris Rybalkin <ribalkin@gmail.com>
2015-01-31 02:36:40 +0000

    fixing args

--- syncloud-verify.py
+++ syncloud-verify.py
@@ -11,15 +11,15 @@ if __name__ == '__main__':
 
     logger.init(logging.DEBUG, True)
 
-    redirect_email = os.environ['REDIRECT_EMAIL']
-    redirect_password = os.environ['REDIRECT_PASSWORD']
+    # redirect_email = os.environ['REDIRECT_EMAIL']
+    # redirect_password = os.environ['REDIRECT_PASSWORD']
 
-    print("login: ".format(redirect_email[:3]))
-    print("password: ".format(redirect_password[:3]))
+    # print("login: ".format(redirect_email[:3]))
+    # print("password: ".format(redirect_password[:3]))
 
     server = get_server()
     release = open('RELEASE', 'r').read().strip()
-    server.activate(release, 'syncloud.info', 'http://api.syncloud.info:81', redirect_email, redirect_password, 'travis')
+    server.activate(release, 'syncloud.info', 'http://api.syncloud.info:81', 'uild@syncloud.it', 'travispassword123', 'travis')
 
     owncloud = facade.get_control(get_insider())
     owncloud.finish('test', 'test', 'localhost', 'http')
