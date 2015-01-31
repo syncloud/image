5722a2632524cbd5290da43838e158a74c0f4d2f
Boris Rybalkin <ribalkin@gmail.com>
2015-01-31 00:21:27 +0000

    adding redirect credentials

--- syncloud-verify.py
+++ syncloud-verify.py
@@ -4,12 +4,16 @@ from syncloud.app import logger
 from syncloud.insider.facade import get_insider
 from syncloud.owncloud import facade
 from syncloud.server.serverfacade import get_server
+import sys
 
 logger.init(logging.DEBUG, True)
 
+redirect_email = sys.argv[6]
+redirect_password = sys.argv[1]
+
 server = get_server()
 release = open('RELEASE', 'r').read().strip()
-server.activate(release, 'localhost', 'http://api.syncloud.info:81', 'mail@example.com', 'test', 'test')
+server.activate(release, 'syncloud.info', 'http://api.syncloud.info:81', redirect_email, redirect_password, 'travis')
 
 owncloud = facade.get_control(get_insider())
 owncloud.finish('test', 'test', 'localhost', 'http')
