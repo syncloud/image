4290ebd40a2aa380915308e92c9995156299e55e
Boris Rybalkin <ribalkin@gmail.com>
2015-01-31 05:24:36 +0000

    logging requests

--- syncloud-verify.py
+++ syncloud-verify.py
@@ -5,12 +5,13 @@ from syncloud.insider.facade import get_insider
 from syncloud.owncloud import facade
 from syncloud.server.serverfacade import get_server
 import responses
+import logging
 
-
-@responses.activate
+# @responses.activate
 def test_install():
 
     logger.init(logging.DEBUG, True)
+    logging.basicConfig(level=logging.DEBUG)
 
     # redirect_email = os.environ['REDIRECT_EMAIL']
     # redirect_password = os.environ['REDIRECT_PASSWORD']
@@ -19,11 +20,11 @@ def test_install():
     # print("password: ".format(redirect_password[:3]))
 
     #test
-    responses.add(responses.POST,
-                      "http://example.com",
-                      status=200,
-                      body='{"user_domain": "travis", "update_token": "some_update_token"}',
-                      content_type="application/json")
+    # responses.add(responses.POST,
+    #                   "http://example.com",
+    #                   status=200,
+    #                   body='{"user_domain": "travis", "update_token": "some_update_token"}',
+    #                   content_type="application/json")
 
     # get_insider().acquire_domain('build@syncloud.it', 'travispassword123', 'travis')
 
