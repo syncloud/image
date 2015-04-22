950fd04f4f5b0728b3913fd27480abb1535e44bf
Boris Rybalkin <ribalkin@gmail.com>
2015-04-22 22:25:36 +0100

    fixing extract

--- extract.sh
+++ extract.sh
@@ -9,16 +9,13 @@ BASE_IMAGE=$1
 PARTED_SECTOR_UNIT=s
 DD_SECTOR_UNIT=b
 OUTPUT=$(echo ${BASE_IMAGE} | cut -d'.' -f1)
-OUTPUT_BOOT_FILE=boot
-OUTPUT_KERNEL_FILES=rootfs
 
-BOOT_PARTITION_END=$(parted -sm ${BASE_IMAGE} unit ${PARTED_SECTOR_UNIT} print | grep "^1" | cut -d ':' -f3 | cut -d 's' -f1)
+BOOT_PARTITION_END_SECTOR=$(parted -sm ${BASE_IMAGE} unit ${PARTED_SECTOR_UNIT} print | grep "^1" | cut -d ':' -f3 | cut -d 's' -f1)
 rm -rf ${OUTPUT}
 mkdir ${OUTPUT}
 
 echo "extracting boot partition with boot loader"
-rm -rf ${OUTPUT_BOOT_FILE}
-dd if=${BASE_IMAGE} of=${OUTPUT}/${OUTPUT_BOOT_FILE} bs=1${DD_SECTOR_UNIT} count=${BOOT_PARTITION_END}
+dd if=${BASE_IMAGE} of=${OUTPUT}/boot bs=1${DD_SECTOR_UNIT} count=$(( ${BOOT_PARTITION_END_SECTOR} - 1 ))
 
 echo "extracting kernel modules and firmware from rootfs"
 
@@ -28,9 +25,9 @@ kpartx -a ${BASE_IMAGE}
 LOOP=$(kpartx -l ${BASE_IMAGE} | head -1 | cut -d ' ' -f1 | cut -c1-5)
 mount /dev/mapper/${LOOP}p2 rootfs
 
-mkdir ${OUTPUT}/${OUTPUT_KERNEL_FILES}
-cp -r rootfs/lib/firmware ${OUTPUT}/${OUTPUT_KERNEL_FILES}/firmware
-cp -r rootfs/lib/modules ${OUTPUT}/${OUTPUT_KERNEL_FILES}/modules
+mkdir ${OUTPUT}/root
+cp -r rootfs/lib/firmware ${OUTPUT}/root/firmware
+cp -r rootfs/lib/modules ${OUTPUT}/root/modules
 sync
 
 umount rootfs
