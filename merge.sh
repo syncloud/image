9a6a395431f20b7a2ce9b04688c4385f929d74bd
Boris Rybalkin <ribalkin@gmail.com>
2015-04-24 19:27:32 +0100

    adding hostname, hosts, cpu, resize

--- merge.sh
+++ merge.sh
@@ -2,6 +2,11 @@
 
 START_TIME=$(date +"%s")
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 board_name"
     exit 1
