560a61b357f82db9180e79b582fefa31e518c063
Boris Rybalkin <ribalkin@gmail.com>
2015-04-24 21:45:54 +0100

    fixing merge logging

--- merge.sh
+++ merge.sh
@@ -16,9 +16,9 @@ if [ ! -d "rootfs" ]; then
     echo "rootfs is not ready, run 'sudo ./rootfs.sh' first"
 fi
 
-echo "========== ${SYNCLOUD_IMAGE} =========="
 
 SYNCLOUD_BOARD=$1
+echo "========== ${SYNCLOUD_BOARD} =========="
 
 RESIZE_PARTITION_ON_FIRST_BOOT=true
 CPU_FREQUENCY_CONTROL=false
