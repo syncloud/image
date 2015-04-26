5f76a1f42b3d25bc4328b7ed8ddff42534fbfd2f
Boris Rybalkin <ribalkin@gmail.com>
2015-04-26 17:42:30 +0100

    fixing apt

--- rootfs-bootstrap.sh
+++ rootfs-bootstrap.sh
@@ -25,18 +25,19 @@ rm -rf rootfs.tar.gz
 qemu-debootstrap --no-check-gpg --include=ca-certificates --arch=armhf wheezy rootfs ${DEB_REPO}
 
 chroot rootfs /bin/bash -c "echo \"root:syncloud\" | chpasswd"
-echo "nameserver 8.8.8.8" > rootfs/run/resolvconf/resolv.conf
+#echo "nameserver 8.8.8.8" > rootfs/run/resolvconf/resolv.conf
 
 chroot rootfs /bin/bash -c "mount -t devpts devpts /dev/pts"
 chroot rootfs /bin/bash -c "mount -t proc proc /proc"
+
+echo "deb http://ftp.us.debian.org/debian wheezy main contrib non-free" > rootfs/etc/apt/sources.list
+echo "deb http://security.debian.org wheezy/updates main contrib non-free" >> rootfs/etc/apt/sources.list
 
 chroot rootfs /bin/bash -c "apt-get update"
 chroot rootfs /bin/bash -c "apt-get -y dist-upgrade"
 chroot rootfs /bin/bash -c "apt-get -y install locales"
 chroot rootfs /bin/bash -c "locale-gen en_US en_US.UTF-8"
-echo "deb http://ftp.us.debian.org/debian wheezy main contrib non-free" rootfs/etc/apt/sources.list
-echo "deb http://security.debian.org wheezy/updates main contrib non-free" rootfs/etc/apt/sources.list
-chroot rootfs /bin/bash -c "apt-get update"
+
 chroot rootfs /bin/bash -c "apt-get -y install openssh-server"
 sed -i "s/^PermitRootLogin .*/PermitRootLogin yes/g" rootfs/etc/ssh/sshd_config
 
