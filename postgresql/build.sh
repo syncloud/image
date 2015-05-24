8acd95deeb06d099c42d114b5038dfa0e599e925
Boris Rybalkin <ribalkin@gmail.com>
2015-05-24 18:59:37 +0100

    fix postgresql build

--- postgresql/build.sh
+++ postgresql/build.sh
@@ -5,7 +5,7 @@ cd ${DIR}
 
 wget https://ftp.postgresql.org/pub/source/v9.4.2/postgresql-9.4.2.tar.bz2
 tar xjvf postgresql-9.4.2.tar.bz2
-cd postgresql
+cd postgresql-9.4.2
 ./configure --prefix /opt/postgresql
 make
 rm -rf /opt/postgresql
