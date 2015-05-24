a2ffe83d090a5afb81d27075f5fbe318a0b4d6b3
Boris Rybalkin <ribalkin@gmail.com>
2015-05-24 19:01:51 +0100

    build tools

--- postgresql/build.sh
+++ postgresql/build.sh
@@ -3,6 +3,8 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
+apt-get install build-essential
+
 wget https://ftp.postgresql.org/pub/source/v9.4.2/postgresql-9.4.2.tar.bz2
 tar xjvf postgresql-9.4.2.tar.bz2
 cd postgresql-9.4.2
