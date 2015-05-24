87ec2e0c0772763ad5b937bcb2997f8d43c684d9
Boris Rybalkin <ribalkin@gmail.com>
2015-05-25 00:40:35 +0100

    nginx

---
+++ nginx/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd ${DIR}
+
+export TMPDIR=/tmp
+export TMP=/tmp
+VERSION=1.8.0
+ROOT=/opt/syncloud-platform
+PREFIX=${ROOT}/nginx
+
+apt-get -y install build-essential flex bison libreadline-dev zlib1g-dev libpcre3-dev
+rm -rf postgresql-${VERSION}.tar.bz2*
+wget http://nginx.org/download/nginx-${VERSION}.tar.gz
+tar xzf nginx-${VERSION}.tar.gz
+cd nginx-${VERSION}
+./configure --prefix ${PREFIX}
+make
+rm -rf ${PREFIX}
+make install
+cd ..
+tar czf nginx-${VERSION}.tar.gz -C ${ROOT} nginx
