776a6c43a55723e6f65adb3135a902ec811911a5
Boris Rybalkin <ribalkin@gmail.com>
2015-06-01 22:53:32 +0100

    moving more apt-get installs to bootstrap

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -82,7 +82,7 @@ chroot rootfs /bin/bash -c "echo 'slapd/root_password password syncloud' | debco
 chroot rootfs /bin/bash -c "echo 'slapd/root_password_again password syncloud' | debconf-set-selections"
 chroot rootfs apt-get -y install sudo openssh-server python-dev gcc wget less bootlogd parted lsb-release unzip bzip2\
  mysql-server libmysqlclient-dev ldap-utils slapd libldap2-dev libsasl2-dev libssl-dev curl dbus avahi-daemon \
- miniupnpc
+ miniupnpc ntp udisks-glue
 
 sed -i "s/^PermitRootLogin .*/PermitRootLogin yes/g" rootfs/etc/ssh/sshd_config
 
