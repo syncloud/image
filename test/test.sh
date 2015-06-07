b7ad40b927bdf3509146f9b96ab958825d9c01e0
Boris Rybalkin <ribalkin@gmail.com>
2015-06-07 01:23:02 +0100

    integration testing by installing all latest apps

--- test/test.sh
+++ test/test.sh
@@ -1,62 +1,22 @@
 #!/bin/bash
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-cd ${DIR}
+APP_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+cd ${APP_DIR}
 
-apt-get install docker.io sshpass
-service docker start
-
-if [ ! -f syncloud-rootfs.tar.gz ]; then
-  echo "syncloud-rootfs.tar.gz is not ready, run 'sudo ./rootfs.sh'"
-  exit 1
+if [[ -n "$3" ]]; then
+    export TEAMCITY_VERSION=$3
 fi
 
-function sshexec {
-    sshpass -p "syncloud" ssh -o StrictHostKeyChecking=no root@localhost -p 2222 "$1"
-}
+#Fix debconf frontend warnings
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export DEBCONF_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive
 
-function cleanup {
+echo "=== install syncloud dependencies ==="
+wget --no-check-certificate --progress=dot:mega -O get-pip.py https://bootstrap.pypa.io/get-pip.py 2>&1
+python get-pip.py
+pip2 install -U pytest
+pip2 install -r dev_requirements.txt
+pip2 install --no-binary :all: syncloud-platform
 
-    echo "cleaning old rootfs"
-    rm -rf rootfs
-
-    echo "docker images"
-    docker images -q
-
-    echo "removing images"
-    docker rm $(docker kill $(docker ps -qa))
-    docker rmi $(docker images -q)
-
-    echo "docker images"
-    docker images -q
-}
-
-cleanup
-
-echo "extracting rootfs"
-tar xzf syncloud-rootfs.tar.gz
-
-echo "rootfs version: $(<rootfs/version)"
-
-cp ../info/RELEASE rootfs/
-cp requirements.txt rootfs/
-cp conftest.py rootfs/
-cp verify.py rootfs/
-
-echo "importing rootfs"
-tar -C rootfs -c . | docker import - syncloud
-
-echo "starting rootfs"
-docker run --name rootfs --privileged -d -it -p 2222:22 syncloud /sbin/init
-
-sleep 3
-
-echo "running tests"
-ssh-keygen -f "/root/.ssh/known_hosts" -R [localhost]:2222
-
-sshexec "echo \"rootfs version (docker):\""
-sshexec "cat /version"
-sshexec "pip2 freeze | grep syncloud"
-sshexec "pip2 install -U pytest"
-sshexec "pip2 install -r /requirements.txt"
-sshexec "cd /; TEAMCITY_VERSION=9 py.test -s verify.py --email=$REDIRECT_EMAIL --password=$REDIRECT_PASSWORD"
+py.test -s test/verify.py --email=$1 --password=$2
--- test/verify.py
+++ test/verify.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python
 import logging
-import convertible
-import pytest
-import requests
+from os.path import dirname
 
+import pytest
 from syncloud.app import logger
 from syncloud.insider.facade import get_insider
 from syncloud.sam.manager import get_sam
-from syncloud.sam.pip import Pip
 from syncloud.server.serverfacade import get_server
 
+DIR = dirname(__file__)
+
+@pytest.fixture(scope="session", autouse=True)
+def activate_device(auth):
+
+    logger.init(logging.DEBUG, True)
+
+    print("installing latest platform")
+    get_sam().update("0.9")
+    get_sam().install("syncloud-platform")
+
+    # persist upnp mock setting
+    get_insider().insider_config.set_upnpc_mock(True)
+
+    server = get_server(insider=get_insider(use_upnpc_mock=True))
+    email, password = auth
+    server.activate('test', 'syncloud.info', 'http://api.syncloud.info:81', email, password, 'teamcity', 'user', 'password', False)
+
+def test_owncloud_activation():
+    # logger.init(logging.DEBUG, True)
+
+    print("installing local owncloud build")
+    get_sam().install("syncloud-owncloud")
+    from syncloud.owncloud.setup import Setup
+
+    setup = Setup()
+    assert not setup.is_finished()
+    setup.finish('test', 'test', overwritehost='localhost')
+    assert setup.is_finished()
 
