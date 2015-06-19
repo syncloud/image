69597d7665299c9fce1785b43c409f354d6c7a3e
Boris Rybalkin <ribalkin@gmail.com>
2015-06-19 22:01:40 +0100

    fixing release

--- integration/test-docker.sh
+++ integration/test-docker.sh
@@ -4,8 +4,8 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
 
-if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
-    echo "usage $0 redirect_user redirect_password redirect_domain"
+if [[ -z "$1" || -z "$2" || -z "$3" || -z "$4" ]]; then
+    echo "usage $0 redirect_user redirect_password redirect_domain release"
     exit 1
 fi
 
@@ -17,6 +17,7 @@ if [[ -n "$TEAMCITY_VERSION" ]]; then
     TC="export TEAMCITY_VERSION=\"$TEAMCITY_VERSION\" ; "
 fi
 
-sshpass -p "syncloud" ssh -o StrictHostKeyChecking=no root@localhost -p 2222 "pip2 install -U pytest"
-sshpass -p "syncloud" ssh -o StrictHostKeyChecking=no root@localhost -p 2222 "pip2 install -r /test/dev_requirements.txt"
-sshpass -p "syncloud" ssh -o StrictHostKeyChecking=no root@localhost -p 2222 "$TC py.test -s /test/integration/verify.py --email=$1 --password=$2 --domain=$3"
+SSH="sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@localhost -p 2222"
+${SSH} "pip2 install -U pytest"
+${SSH} "pip2 install -r /test/dev_requirements.txt"
+${SSH} "$TC py.test -s /test/integration/verify.py --email=$1 --password=$2 --domain=$3 --release=$4"
