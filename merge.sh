e802d8336cae45c52385776c79cde6ab6e98ad36
Boris Rybalkin <ribalkin@gmail.com>
2015-07-15 20:51:48 +0100

    fixing dependency url

--- merge.sh
+++ merge.sh
@@ -15,7 +15,7 @@ SYNCLOUD_BOARD=$1
 echo "========== ${SYNCLOUD_BOARD} =========="
 
 if [ ! -f "rootfs.tar.gz" ]; then
-    wget http://build.syncloud.org:8111/guestAuth/repository/download/${SYNCLOUD_BOARD}_rootfs_armv7l/lastSuccessful/rootfs.tar.gz\
+    wget http://build.syncloud.org:8111/guestAuth/repository/download/${SYNCLOUD_BOARD}_rootfs_syncloud_armv7l/lastSuccessful/rootfs.tar.gz\
   -O rootfs.tar.gz --progress dot:giga
 elae
     echo "rootfs.tar.gz is here"
