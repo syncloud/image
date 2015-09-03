a9bb7a2474a15afe8d45f739b90b8d8a2727d01a
Boris Rybalkin <ribalkin@gmail.com>
2015-09-03 06:41:24 +0100

    removing lshw as we are using lsblk now

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -83,7 +83,7 @@ chroot ${ROOTFS} apt-get update
 chroot ${ROOTFS} apt-get -y dist-upgrade
 chroot ${ROOTFS} apt-get -y dpkg-dev
 chroot ${ROOTFS} apt-get -y install sudo openssh-server wget less bootlogd parted lsb-release unzip bzip2\
- libldap2-dev libsasl2-dev libssl-dev curl dbus avahi-daemon ntp libpq-dev lshw udisks2
+ libldap2-dev libsasl2-dev libssl-dev curl dbus avahi-daemon ntp libpq-dev udisks2
 chroot ${ROOTFS} ssh-keygen -f /root/.ssh/id_rsa -t rsa -N ''
 chroot ${ROOTFS} /bin/bash -c "cat /root/.ssh/id_rsa.pub > /root/.ssh/authorized_keys"
 
