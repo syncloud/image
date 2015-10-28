3ab0c8820d8718b44d1c38256dbdc37c54430a05
Boris Rybalkin <ribalkin@gmail.com>
2015-10-28 01:18:56 +0000

    adding xu3/xu3lite/xu4

--- extract/extract.sh
+++ extract/extract.sh
@@ -61,7 +61,11 @@ elif [[ ${SYNCLOUD_BOARD} == "cubietruck" ]]; then
   CPU_FREQUENCY_GOVERNOR=performance
   CPU_FREQUENCY_MAX=1056000
   CPU_FREQUENCY_MIN=648000
-#elif [[ ${SYNCLOUD_BOARD} == "odroid-xu3" ]];
+elif [[ ${SYNCLOUD_BOARD} == "odroid-xu3" ]];
+  IMAGE_FILE="/tmp/ubuntu-15.04-mate-odroid-xu3-20150710.img"
+  IMAGE_FILE_ZIP=${IMAGE_FILE}.xz
+  DOWNLOAD_IMAGE="wget --progress=dot:giga http://dn.odroid.com/5422/ODROID-XU3/Ubuntu/ubuntu-15.04-mate-odroid-xu3-20150710.img.xz -O $IMAGE_FILE_ZIP"
+  UNZIP=unxz
 elif [[ ${SYNCLOUD_BOARD} == "bananapim2" ]]; then
   IMAGE_FILE="/tmp/M2-raspberry-kernel3.3-LCD.img"
   IMAGE_FILE_ZIP=${IMAGE_FILE}.zip
