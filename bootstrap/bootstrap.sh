e1a620830e14a9845035831e00920340a8b6ab65
Boris Rybalkin <ribalkin@gmail.com>
2016-12-28 18:04:16 +0000

    removing more libs

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -84,8 +84,7 @@ fi
 
 chroot ${ROOTFS} apt-get update
 chroot ${ROOTFS} apt-get -y dist-upgrade
-chroot ${ROOTFS} apt-get -y install sudo openssh-server wget less bootlogd parted lsb-release unzip bzip2\
-  libssl-dev curl dbus avahi-daemon ntp
+chroot ${ROOTFS} apt-get -y install sudo openssh-server wget less parted lsb-release unzip bzip2 curl dbus avahi-daemon ntp
 chroot ${ROOTFS} ssh-keygen -f /root/.ssh/id_rsa -t rsa -N ''
 chroot ${ROOTFS} /bin/bash -c "cat /root/.ssh/id_rsa.pub > /root/.ssh/authorized_keys"
 sed -i -e'/AVAHI_DAEMON_DETECT_LOCAL/s/1/0/' ${ROOTFS}/etc/default/avahi-daemon
