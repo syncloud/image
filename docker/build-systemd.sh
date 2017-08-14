0aa788481f82ea6ac2359b93966d9c9bf0bb17fa
Boris Rybalkin <ribalkin@gmail.com>
2017-08-14 23:23:34 +0100

    fix docker image build [skip ci]

--- docker/build-systemd.sh
+++ docker/build-systemd.sh
@@ -9,7 +9,7 @@ if [ -z "$1" ]; then
 fi
 
 ARCH=$1
-
+apt-get install debootstrap
 ${DIR}/../bootstrap/bootstrap.sh
 cat rootfs-$(dpkg --print-architecture).tar.gz | docker import - syncloud/rootfs-${ARCH}
 docker build -f Dockerfile.systemd.${ARCH} -t syncloud/systemd-${ARCH} .
