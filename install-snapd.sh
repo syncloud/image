97204db64f2257937480dc607b51d110f1faabe3
Boris Rybalkin <ribalkin@gmail.com>
2017-08-28 10:26:11 +0100

    upgrade snapd to 170828

--- install-snapd.sh
+++ install-snapd.sh
@@ -8,7 +8,7 @@ if [[ -z "$1" ]]; then
 fi
 
 ARCH=$(dpkg --print-architecture)
-VERSION=170819
+VERSION=170828
 
 SNAPD=snapd-${VERSION}-${ARCH}.tar.gz
 wget http://apps.syncloud.org/apps/${SNAPD} --progress=dot:giga
