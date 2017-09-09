3e4a64eacd521b948929731216d8e625462c7b4a
Boris Rybalkin <ribalkin@gmail.com>
2017-09-09 15:37:30 +0100

    silence fsck

--- extract/extract.sh
+++ extract/extract.sh
@@ -216,7 +216,7 @@ else
         
         #lsof | grep ${LOOP}p1
         #lsof | grep ${BOOT}
-        fsck -fy /dev/mapper/${LOOP}p1
+        fsck -fy /dev/mapper/${LOOP}p1 | true
         
         resize2fs /dev/mapper/${LOOP}p1 100M
         
