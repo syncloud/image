d1ed77fc57d936fc395018489a5ad497f30ee339
Boris Rybalkin <ribalkin@gmail.com>
2017-09-16 07:20:54 +0100

    fix merge

--- merge.sh
+++ merge.sh
@@ -91,8 +91,6 @@ ROOTFS_START_SECTOR=$(( ${BOOT_SECTORS} + 1  ))
 ROOTFS_SECTORS=$(( ${ROOTFS_SIZE_BYTES} / 512 ))
 ROOTFS_END_SECTOR=$(( ${ROOTFS_START_SECTOR} + ${ROOTFS_SECTORS} - 2 ))
 parted -sm ${SYNCLOUD_IMAGE} print | tee parted.out
-PARTITIONS=$( cat parted.out | tail -n +3 | wc -l)
-if [ ${PARTITIONS} == 2 ]; then
 
 echo "creating defining second partition (${ROOTFS_START_SECTOR} - ${ROOTFS_END_SECTOR}) sectors"
 echo "
