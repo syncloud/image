26f2fe2f9dc6061c7d2cc3b57cf3bec294e44e08
Boris Rybalkin <ribalkin@gmail.com>
2017-10-25 18:39:49 +0100

    fsck before mounting

--- extract/extract.sh
+++ extract/extract.sh
@@ -318,7 +318,17 @@ if [ ${PARTITIONS} == 2 ]; then
     rm -rf ${ROOTFS}
     mkdir -p ${ROOTFS}
     ROOTFS_LOOP=${LOOP}p2
+    sync
     blkid /dev/mapper/${ROOTFS_LOOP} -s UUID -o value > uuid
+    set +e
+    fsck -fy /dev/mapper/${ROOTFS_LOOP}
+    if [ "$?" != 0 ]; then
+        sleep 5
+        echo "retrying"
+        fsck -fy /dev/mapper/${ROOTFS_LOOP}
+    fi
+    set -e
+
     mount /dev/mapper/${ROOTFS_LOOP} ${ROOTFS}
     mount | grep ${ROOTFS}
 
