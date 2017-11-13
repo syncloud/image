ad0b3ef19a3a58b5626b57dca03840d5c17bc83d
Boris Rybalkin <ribalkin@gmail.com>
2017-11-13 23:28:10 +0000

    no s3 upload anymore (github releases now) [skip ci]

--- tools/upload.sh
+++ tools/upload.sh
@@ -7,27 +7,22 @@ file=$2
 CHANNEL=$3
 bucket=image.syncloud.org
 
-if [ "${CHANNEL}" == "stable" ] ; then
-  
-    s3cmd put ${file} s3://${bucket}/${release}/${file}
+#if [ "${CHANNEL}" == "stable" ] ; then
+#TODO implement github release upload
 
-else
+set +x # do not display ssh key in the log
 
-    set +x # do not display ssh key in the log
-
-    if [ -z "$ARTIFACT_SSH_KEY" ]; then
-        echo "ARTIFACT_SSH_KEY must be set"
-        exit 1
-    fi
-
-    echo "$ARTIFACT_SSH_KEY" | base64 --decode > artifact_ssh.key
-    chmod 600 artifact_ssh.key
-    chmod -R a+r ${file}
-
-    scp -r -oStrictHostKeyChecking=no -i artifact_ssh.key ${file} \
-        artifact@artifact.syncloud.org:/home/artifact/repo/image/${file}
-
-    set -x
-
+if [ -z "$ARTIFACT_SSH_KEY" ]; then
+    echo "ARTIFACT_SSH_KEY must be set"
+    exit 1
 fi
+
+echo "$ARTIFACT_SSH_KEY" | base64 --decode > artifact_ssh.key
+chmod 600 artifact_ssh.key
+chmod -R a+r ${file}
+
+scp -r -oStrictHostKeyChecking=no -i artifact_ssh.key ${file} \
+    artifact@artifact.syncloud.org:/home/artifact/repo/image/${file}
+
+set -x
 
