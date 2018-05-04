8a63db7d6f9a3020e24074ab3db2bc362c90cc8e
Boris Rybalkin <ribalkin@gmail.com>
2018-05-04 14:54:18 +0100

    snapd 180503

--- tools/install-snapd.sh
+++ tools/install-snapd.sh
@@ -8,7 +8,7 @@ if [[ -z "$1" ]]; then
 fi
 
 ARCH=$(dpkg --print-architecture)
-VERSION=170828
+VERSION=180503
 
 SNAPD=snapd-${VERSION}-${ARCH}.tar.gz
 wget http://apps.syncloud.org/apps/${SNAPD} --progress=dot:giga
