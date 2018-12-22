c76171fb07032c27e2a9b230b8df681984ec5f43
Boris Rybalkin <ribalkin@gmail.com>
2018-12-22 20:55:17 +0000

    latest rock64

--- tools/extract.sh
+++ tools/extract.sh
@@ -102,7 +102,7 @@ elif [[ ${SYNCLOUD_BOARD} == "bananapim3" ]]; then
   DOWNLOAD_IMAGE="wget --progress=dot:giga ${SYNCLOUD_DISTR_URL}/${IMAGE_FILE}.xz -O $IMAGE_FILE_ZIP"
   UNZIP=unxz
 elif [[ ${SYNCLOUD_BOARD} == "rock64" ]]; then
-  IMAGE_FILE="Armbian_5.59_Rock64_Debian_stretch_default_4.4.152_desktop.img"
+  IMAGE_FILE="Armbian_5.65_Rock64_Debian_stretch_default_4.4.162_desktop.img"
   IMAGE_FILE_ZIP=${IMAGE_FILE}.xz
   DOWNLOAD_IMAGE="wget --progress=dot:giga ${SYNCLOUD_DISTR_URL}/${IMAGE_FILE}.xz -O $IMAGE_FILE_ZIP"
   UNZIP=unxz
