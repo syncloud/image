4745285b9f4ba930e16be0a9783e9c15a594d0f9
Boris Rybalkin <ribalkin@gmail.com>
2021-01-16 19:32:28 +0000

    fix odroid hc4

--- tools/extract.sh
+++ tools/extract.sh
@@ -80,7 +80,7 @@ elif [[ ${SYNCLOUD_BOARD} == "odroid-u3" ]]; then
   IMAGE_FILE_ZIP=${IMAGE_FILE}.xz
   DOWNLOAD_IMAGE="wget --progress=dot:giga ${SYNCLOUD_DISTR_URL}/${IMAGE_FILE_ZIP}"
 elif [[ ${SYNCLOUD_BOARD} == "odroid-hc4" ]]; then
-  IMAGE_FILE="ubuntu-20.04-4.9-minimal-odroid-c4-hc4-20201212.img.xz"
+  IMAGE_FILE="ubuntu-20.04-4.9-minimal-odroid-c4-hc4-20201212.img"
   IMAGE_FILE_ZIP=${IMAGE_FILE}.xz
   DOWNLOAD_IMAGE="wget --progress=dot:giga ${SYNCLOUD_DISTR_URL}/${IMAGE_FILE_ZIP}"
 elif [[ ${SYNCLOUD_BOARD} == "bananapim2" ]]; then
