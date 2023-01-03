4f48a483985a8242abda132211faf1a13b57f9a1
viscropst <viscropst@petalmail.com>
2023-01-03 12:50:57 +0800

    Make loop device detach syncloud's only

--- cleanup.sh
+++ cleanup.sh
@@ -5,10 +5,12 @@ echo "===== loop cleanup ====="
 ls -la /dev/mapper/*
 losetup || true
 
-mapper_arr=(`ls /dev/mapper/loop* 2>/dev/null`)
+mapper_arr=(`ls /dev/mapper/loop* 2>/dev/null || true`)
 if [[ ${#mapper_arr[@]} -gt 0 ]];then
  apt update && apt install -y dmsetup
  dmsetup remove -f /dev/mapper/loop* || true
 fi
 
-losetup -D || true
+
+loopdev_name=`losetup -O NAME,BACK-FILE | grep syncloud-*-$DRONE_TAG.img | cut -d " " -f 1`
+[[ -n $loopdev_name ]] && losetup -d $loopdev_name || true
