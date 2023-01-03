f364edf088810c752bcc3ea7b9db73d025078a7a
viscropst <viscropst@petalmail.com>
2023-01-03 11:54:36 +0800

    Make cleanup to remove exist loop device mapping then detach the loop device

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -e
 
 echo "===== loop cleanup ====="
-apt update && apt install -y dmsetup
+
 ls -la /dev/mapper/*
 losetup || true
-dmsetup remove -f /dev/mapper/loop* || true
+
+mapper_arr=(`ls /dev/mapper/loop* 2>/dev/null`)
+if [[ ${#mapper_arr[@]} -gt 0 ]];then
+ apt update && apt install -y dmsetup
+ dmsetup remove -f /dev/mapper/loop* || true
+fi
+
+losetup -D || true
