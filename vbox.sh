b9655e16dbeda1b0600994e05425830ca969196b
Boris Rybalkin <ribalkin@gmail.com>
2024-06-01 23:11:49 +0100

    fix vbox scp

--- vbox.sh
+++ vbox.sh
@@ -9,8 +9,8 @@ IMAGE_NAME=$1
 DISTRO=$2
 
 echo "$KEY" > ssh.key
-cp -i ssh.key $IMAGE.img root@$HOST:/data/drone-$DISTRO
-cp -i ssh.key create_vbox_image.sh root@$HOST:/data/drone-$DISTRO
+scp -i ssh.key $IMAGE.img root@$HOST:/data/drone-$DISTRO
+scp -i ssh.key create_vbox_image.sh root@$HOST:/data/drone-$DISTRO
 ssh -i ssh.key root@$HOST:/data/drone-$DISTRO/create_vbox_image.sh $IMAGE_NAME
-cp -i ssh.key create_vbox_image.sh root@$HOST:/data/drone-$DISTRO/${IMAGE_NAME}.xz .
+scp -i ssh.key create_vbox_image.sh root@$HOST:/data/drone-$DISTRO/${IMAGE_NAME}.xz .
 
