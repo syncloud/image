188a15a2130b29146d59889e824d7f34b76543f2
Boris Rybalkin <ribalkin@gmail.com>
2024-06-01 23:50:44 +0100

    ssh fix

--- vbox.sh
+++ vbox.sh
@@ -10,8 +10,8 @@ IMAGE_NAME=$1
 DISTRO=$2
 
 echo "$KEY" > ssh.key
-scp -i ssh.key $IMAGE.img root@$HOST:/data/drone-$DISTRO
-scp -i ssh.key create_vbox_image.sh root@$HOST:/data/drone-$DISTRO
-ssh -i ssh.key root@$HOST:/data/drone-$DISTRO/create_vbox_image.sh $IMAGE_NAME
-scp -i ssh.key create_vbox_image.sh root@$HOST:/data/drone-$DISTRO/${IMAGE_NAME}.xz .
+scp -i ssh.key  -o StrictHostKeyChecking=no $IMAGE.img root@$HOST:/data/drone-$DISTRO
+scp -i ssh.key  -o StrictHostKeyChecking=no create_vbox_image.sh root@$HOST:/data/drone-$DISTRO
+ssh -i ssh.key  -o StrictHostKeyChecking=no root@$HOST:/data/drone-$DISTRO/create_vbox_image.sh $IMAGE_NAME
+scp -i ssh.key  -o StrictHostKeyChecking=no create_vbox_image.sh root@$HOST:/data/drone-$DISTRO/${IMAGE_NAME}.xz .
 
