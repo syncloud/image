83397f2432abb3a11864792e347ad366fc875ff3
Boris Rybalkin <ribalkin@gmail.com>
2024-06-02 13:13:25 +0100

    fix image

--- vbox.sh
+++ vbox.sh
@@ -6,13 +6,12 @@ apt update
 apt install -y ssh
 
 
-IMAGE_NAME=$1
+IMAGE=$1
 DISTRO=$2
 
 echo "$KEY" > ssh.key
 chmod 600 ssh.key
 scp -i ssh.key  -o StrictHostKeyChecking=no $IMAGE.img root@$HOST:/data/drone-$DISTRO
 scp -i ssh.key  -o StrictHostKeyChecking=no create_vbox_image.sh root@$HOST:/data/drone-$DISTRO
-ssh -i ssh.key  -o StrictHostKeyChecking=no root@$HOST:/data/drone-$DISTRO/create_vbox_image.sh $IMAGE_NAME
-scp -i ssh.key  -o StrictHostKeyChecking=no create_vbox_image.sh root@$HOST:/data/drone-$DISTRO/${IMAGE_NAME}.xz .
-
+ssh -i ssh.key  -o StrictHostKeyChecking=no root@$HOST:/data/drone-$DISTRO/create_vbox_image.sh $IMAGE
+scp -i ssh.key  -o StrictHostKeyChecking=no create_vbox_image.sh root@$HOST:/data/drone-$DISTRO/${IMAGE}.vdi.xz .
